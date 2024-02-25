6209dd6cb5edb7119e2a6a230bd65ea6be526e60
LeeSiny <sinyoung.siny@gmail.com>
2024-02-25 22:54:18 +0900

    Java - Tricount

---
+++ tricount/src/main/java/com/goorm/tricount/TricountApplication.java
@@ -0,0 +1,11 @@
+package com.goorm.tricount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TricountApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TricountApplication.class, args);
+    } //main
+} //class
